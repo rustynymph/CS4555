ecd1a819e70f3c976786db0a921c2852c32f1fb0
Annie Kelly <annie.kelly@colorado.edu>
2015-01-21 15:49:29 -0700

    blerg

--- compile.py
+++ compile.py
@@ -4,7 +4,7 @@ from compiler.ast import *
 import compiler
 import sys
 import string
-from python_compiler import *
+from Flatten import *
 
 ast = compiler.parse("x = (4 + 1) + - 2; print 3 + ( 5 + 2 + x) +  - (1 + input() )")
 python_compiler.treeFlatten(ast)
