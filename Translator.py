cc187a57173da665e6d4d1259ef21105a72c2b88
Andrew Arnopoulos <andrewarnopoulos@gmail.com>
2015-01-22 16:02:25 -0700

    Updated Translator

--- Translator.py
+++ Translator.py
@@ -5,9 +5,22 @@ class Translator:
 
 	@staticmethod
 	def pythonASTToAssemblyAST(ast):
-		if isinstance(ast,Module): return AssemblyProgram([Translator.pythonASTToAssemblyAST(ast.node)])
-		elif isinstance(ast,Stmt): return AssemblyFunction("main",[Translator.pythonASTToAssemblyAST(n) for n in ast.nodes])
-		elif isinstance(ast,Discard): return Translator.pythonASTToAssemblyAST(ast.expr)
-		elif isinstance(ast,Const): return ConstantOperand(ast.value)
+		memory = {}
+		hasEntry = False
+		def translatePythonAST(ast):
+			if isinstance(ast,Module): return AssemblyProgram([Translator.pythonASTToAssemblyAST(ast.node)])
+			elif isinstance(ast,Stmt): 
+				if hasEntry:
+					hasEntry = True
+					return AssemblyFunction("main",[Translator.pythonASTToAssemblyAST(n) for n in ast.nodes])
+				else: [Translator.pythonASTToAssemblyAST(n) for n in ast.nodes]
+			elif isinstance(ast,Discard): return Translator.pythonASTToAssemblyAST(ast.expr)
+			elif isinstance(ast,Const): return ConstantOperand(ast.value)
+			elif isinstance(ast,Assign):
+				offset = 4*(len(memory)+1)
+				memory[ast.nodes[0].name] = offset
+				if isinstance(ast.expr,Const):
 
-		return "Error: " + str(ast) + " currently not supported.\n"
+			return "Error: " + str(ast) + " currently not supported.\n"
+
+		return translatePythonAST(ast)
