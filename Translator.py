a1b8ce30cce4c1669458d2a8569875142277d5e7
Andrew Arnopoulos <andrewarnopoulos@gmail.com>
2015-01-23 22:55:51 -0700

    21 down 8 to go

--- Translator.py
+++ Translator.py
@@ -6,7 +6,10 @@ class Translator:
 	@staticmethod
 	def pythonASTToAssemblyAST(ast):
 		memory = {}
+		def getVariableInMemory(name):
+			return MemoryOperand(RegisterOperand("ebp"),memory[name])
 		def translatePythonAST(ast):
+			print ast
 			if isinstance(ast,Module): return AssemblyProgram([translatePythonAST(ast.node)])
 			elif isinstance(ast,Stmt):
 				x86 = []
@@ -16,50 +19,41 @@ class Translator:
 			elif isinstance(ast,Discard): return translatePythonAST(ast.expr)
 			elif isinstance(ast,Const): return [ConstantOperand(ast.value)]
 			elif isinstance(ast,Assign):
-				offset = -4*(len(memory)+1)
-				memory[ast.nodes[0].name] = offset
-				subAST = translatePythonAST(ast.expr)
-				assign = MoveInstruction(RegisterOperand("eax"),MemoryOperand(RegisterOperand("ebp"),offset),"l")
-				if len(subAST) == 1: 
-					# print MoveInstruction(subAST[0],MemoryOperand(RegisterOperand("ebp"),offset),"l").printInstruction()
-
-					if isinstance(subAST[0],MemoryOperand):
-						instructions = []
-						instructions += [MoveInstruction(subAST[0],RegisterOperand("eax"),"l")]
-						instructions += [MoveInstruction(RegisterOperand("eax"),MemoryOperand(RegisterOperand("ebp"),offset),"l")]
-						return instructions
-					elif isinstance(subAST[0],CallInstruction): return subAST + [MoveInstruction(RegisterOperand("eax"),MemoryOperand(RegisterOperand("ebp"),offset),"l")]
-					elif isinstance(subAST[0],ConstantOperand): return [MoveInstruction(subAST[0],RegisterOperand("eax"),"l")]
-					elif isinstance(subAST[0],NegativeInstruction): return subAST + [MoveInstruction(RegisterOperand("eax"),MemoryOperand(RegisterOperand("ebp"),offset),"l")]
-					else: 
-						
-						return [MoveInstruction(subAST[0],MemoryOperand(RegisterOperand("ebp"),offset),"l")]
-				else: return translatePythonAST(ast.expr) + [assign]
-			elif isinstance(ast,Name): return [MemoryOperand(RegisterOperand("ebp"),memory[ast.name])]
+				assignInstruction = translatePythonAST(ast.nodes[0])[0]
+				x86AST = translatePythonAST(ast.expr)
+				print "hellofdsa: " + str(assignInstruction)
+				if len(x86AST) == 1:
+					if isinstance(x86AST[0],Operand):
+						x86AST = [MoveInstruction(x86AST[0],RegisterOperand("eax"),"l")]
+					return x86AST + [MoveInstruction(RegisterOperand("eax"),assignInstruction,"l")]
+				return x86AST + [MoveInstruction(RegisterOperand("eax"),assignInstruction,"l")]
+			elif isinstance(ast,AssName):
+				memory[ast.name] = -4*(len(memory)+1)
+				return [getVariableInMemory(ast.name)]
+			# elif isinstance(ast,Name): return [MoveInstruction(getVariableInMemory(ast.name),RegisterOperand("eax"),"l")]
+			elif isinstance(ast,Name): return [getVariableInMemory(ast.name)]
 			elif isinstance(ast,CallFunc): return [CallInstruction(FunctionCallOperand(ast.node.name))]
 			elif isinstance(ast,Printnl):
-				i = [PushInstruction(MemoryOperand(RegisterOperand("ebp"),memory[ast.nodes[0].name]),"l")]
+				print ast.nodes[0].name
+				i = [PushInstruction(getVariableInMemory(ast.nodes[0].name),"l")]
 				i += [CallInstruction(FunctionCallOperand("print_int_nl"))]
 				i += [AddInstruction(ConstantOperand(4),RegisterOperand("esp"),"l")]
 				return i
 			elif isinstance(ast,UnarySub):
-				if isinstance(ast.expr,Name):
-					i = [MoveInstruction(MemoryOperand(RegisterOperand("ebp"),memory[ast.expr.name]),RegisterOperand("eax"),"l")]
-					i = [NegativeInstruction(RegisterOperand("eax"),"l")]
-					return i
-				elif isinstance(ast.expr,Const):
-					i = [MoveInstruction(ConstantOperand(ast.expr.value),RegisterOperand("eax"),"l")]
-					i = [NegativeInstruction(RegisterOperand("eax"),"l")]
-					return i
+				instruction = translatePythonAST(ast.expr)[0]
+				instruction = [MoveInstruction(instruction,RegisterOperand("eax"),"l")]
+				return instruction + [NegativeInstruction(RegisterOperand("eax"),"l")]
 			elif isinstance(ast,Add):
-				i = []
-				if isinstance(ast.left,Name): i += [MoveInstruction(MemoryOperand(RegisterOperand("ebp"),memory[ast.left.name]),RegisterOperand("eax"),"l")]
-				else: i += [MoveInstruction(ConstantOperand(ast.left.value),RegisterOperand("eax"),"l")]
-
-				if isinstance(ast.right,Name): i += [AddInstruction(MemoryOperand(RegisterOperand("ebp"),memory[ast.right.name]),RegisterOperand("eax"),"l")]
-				else: [AddInstruction(ConstantOperand(ast.right.value,RegisterOperand("eax"),"l"))]
-
-				return i
+				leftAST = translatePythonAST(ast.left)
+				rightAST = translatePythonAST(ast.right)
+				leftAST = [MoveInstruction(leftAST[0],RegisterOperand("eax"),"l")]
+				rightAST = [AddInstruction(rightAST[0],RegisterOperand("eax"),"l")]
+				# print leftAST + rightAST
+				for i in leftAST + rightAST:
+					print i.printInstruction()
+				return leftAST + rightAST
 
 			raise "Error: " + str(ast) + " currently not supported.\n"
-		return translatePythonAST(ast)
+		t = translatePythonAST(ast)
+		print memory
+		return t
