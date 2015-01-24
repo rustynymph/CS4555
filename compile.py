62a21466ca02e8bcd36bc7ee25cc922a892e19d0
Andrew Arnopoulos <andrewarnopoulos@gmail.com>
2015-01-24 01:41:13 -0700

    Removed commented code

--- compile.py
+++ compile.py
@@ -10,30 +10,13 @@ from Optimizer import *
 from Parser import *
 from AssemblyAST import *
 
-# ast = compiler.parse("print -5")
-# flat_ast = python_compiler.treeFlatten(ast)
-# x86_ast = Translator.pythonASTToAssemblyAST(flat_ast)
-# #print x86_ast
-# print x86_ast
-# print x86_ast.printInstruction()
-
 pythonFilename = sys.argv[1]
 pythonAST = Parser.parseFile(pythonFilename)
 pythonAST = Optimizer.reduce(pythonAST)
+pythonAST = Optimizer.negation(pythonAST)
 flattenedAST = python_compiler.treeFlatten(pythonAST)
 x86AST = Translator.pythonASTToAssemblyAST(flattenedAST)
 
 x86Filename = sys.argv[1].rsplit(".",1)[0] + ".s"
 x86File = open(x86Filename,"w")
 x86File.write(x86AST.printInstruction())
-
-'''
-filename = sys.argv[1]
-prefix = filename[:len(filename)-3] #takes the prefix name from our *.py file
-ast = compiler.parseFile(filename)
-flat_ast = python_compiler.treeFlatten(ast)
-x86_ast = Translator.pythonASTToAssemblyAST(flat_ast)
-file = open(prefix+".s","w")
-file.write(x86_ast)
-file.close()
-'''
