4499d1626a24fd48b506e43162b23e9e2fcf5c3b
Andrew Arnopoulos <andrewarnopoulos@gmail.com>
2015-02-12 15:36:26 -0700

    Compiling incorrectly

--- Translator.py
+++ Translator.py
@@ -16,67 +16,76 @@ class Translator:
 		print("\n")
 
 		def getVariableInMemory(name): #update this
-			if name in coloredgraph:
-				if coloredgraph[name] not in colors:
-					return MemoryOperand(RegisterOperand("ebp"),coloredgraph[name])
-				else:
-					return RegisterOperand(coloredgraph[name])
-			#return MemoryOperand(RegisterOperand("ebp"),memory[name])
+			# if name in coloredgraph:
+			# 	if coloredgraph[name] not in colors:
+			# 		return MemoryOperand(RegisterOperand("ebp"),coloredgraph[name])
+			# 	else:
+			# 		return RegisterOperand(coloredgraph[name])
+			return MemoryOperand(RegisterOperand("ebp"),memory[name])
 			
 		def translatePythonAST(ast,liveness=None):
 			spill_vars = 0
-			if isinstance(ast,Module): return AssemblyProgram([translatePythonAST(ast.node)])
+			if isinstance(ast,Module): return AssemblyProgram([translatePythonAST(ast.node,liveness)])
 			
 			elif isinstance(ast,Stmt):
-				x86 = []
-				for n in ast.nodes:
-					x86 += translatePythonAST(n)
+				if isinstance(liveness,list): x86 = [translatePythonAST(ast.nodes[i],liveness[i:i+2]) for i in range(len(ast.nodes)-1)]
+				else: x86 = [translatePythonAST(n) for n in ast.nodes]
 				return AssemblyFunction("main",x86,4*(len(memory)+1))
 				
-			elif isinstance(ast,Discard): return translatePythonAST(ast.expr)
+			elif isinstance(ast,Discard): return translatePythonAST(ast.expr,liveness)
 			
 			elif isinstance(ast,Const): return ConstantOperand(ast.value)
 			
 			elif isinstance(ast,Assign):
-				assignInstruction = translatePythonAST(ast.nodes[0])
-				x86AST = translatePythonAST(ast.expr)
-				if len(x86AST) == 1:
-					if isinstance(x86AST[0],Operand):
-						x86AST = [MoveInstruction(x86AST[0],assignInstruction,"l")]
-					return x86AST[0]
-				return x86AST[0]
+
+				assignInstruction = translatePythonAST(ast.nodes[0],liveness)
+				x86AST = translatePythonAST(ast.expr,liveness)
+				if isinstance(x86AST,Operand):
+					x86AST = MoveInstruction(x86AST,RegisterOperand("eax"),"l")
+				return ClusteredInstructions([x86AST] + [MoveInstruction(RegisterOperand("eax"),assignInstruction,"l")])
+
+
 			
 			elif isinstance(ast,AssName):				
 				
 				#need to change this, assign to registers
+				if ast.name not in memory: memory[ast.name] = -4*(len(memory)+1)
 				if ast.name in coloredgraph:
 					register = coloredgraph[ast.name]
-
-				return RegisterOperand(register)
+					return RegisterOperand(register)
+				else: return getVariableInMemory(ast.name)
 				
 			elif isinstance(ast,Name): return getVariableInMemory(ast.name)
 			
-			elif isinstance(ast,CallFunc): return CallInstruction(FunctionCallOperand(ast.node.name))
+			elif isinstance(ast,CallFunc):
+				call = CallInstruction(FunctionCallOperand(ast.node.name))
+				if isinstance(liveness,list):
+					saveToDisk = [MoveInstruction(coloredgraph[x],getVariableInMemory(x),"l") for x in liveness[0]]
+					saveToRegister = [MoveInstruction(getVariableInMemory(x),coloredgraph[x],"l") for x in liveness[1]]
+					return ClusteredInstructions(saveToDisk + [call] + saveToRegister)
+				else: return call
 			
 			elif isinstance(ast,Printnl):
-				i = [PushInstruction(getVariableInMemory(ast.nodes[0].name),"l")]
+				operand = getVariableInMemory(ast.nodes[0].name)
+				if ast.nodes[0].name in coloredgraph: operand = RegisterOperand(coloredgraph[ast.nodes[0].name])
+				i = [PushInstruction(operand,"l")]
 				i += [CallInstruction(FunctionCallOperand("print_int_nl"))]
 				i += [AddInstruction(ConstantOperand(4),RegisterOperand("esp"),"l")]
 				return ClusteredInstructions(i)
 			
 			elif isinstance(ast,UnarySub):
-				instruction = translatePythonAST(ast.expr)
+				instruction = translatePythonAST(ast.expr,liveness)
 				instruction = [MoveInstruction(instruction,RegisterOperand("eax"),"l")]
 				return ClusteredInstructions(instruction + [NegativeInstruction(RegisterOperand("eax"),"l")])
 			
 			elif isinstance(ast,Add):
-				leftAST = translatePythonAST(ast.left)
-				rightAST = translatePythonAST(ast.right)
+				leftAST = translatePythonAST(ast.left,liveness)
+				rightAST = translatePythonAST(ast.right,liveness)
 				leftAST = [MoveInstruction(leftAST,RegisterOperand("eax"),"l")]
 				rightAST = [AddInstruction(rightAST,RegisterOperand("eax"),"l")]
 				return ClusteredInstructions(leftAST + rightAST)
 			
 			raise "Error: " + str(ast) + " currently not supported.\n"
 			
-		t = translatePythonAST(ast)
+		t = translatePythonAST(ast,la)
 		return t
