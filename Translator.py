e03fd2716d65133883d32b6e2e6862559a96644e
shadowfax6894 <anniekelly6894@gmail.com>
2015-02-20 14:34:37 -0700

    more cleaning

--- Translator.py
+++ Translator.py
@@ -44,7 +44,7 @@ class Translator:
 						save_name = x
 						return save_name
 		
-		def spillName(name,val,liveness,current_instruction):
+		def spillName(name,val,liveness):
 
 			save_name = checkName("ebx",0)
 								 
@@ -54,7 +54,7 @@ class Translator:
 			load_instruction = MoveInstruction(getVariableInMemory(save_name),RegisterOperand("ebx"),"l")
 			return ClusteredInstructions([save_instruction,mem_mov1,mem_mov2,load_instruction])
 		
-		def spillUnary(name,val,liveness,current_instruction):
+		def spillUnary(name,val,liveness):
 
 			save_name = checkName("ebx",0)
 			
@@ -65,7 +65,7 @@ class Translator:
 			load_instruction = MoveInstruction(getVariableInMemory(save_name),RegisterOperand("ebx"),"l")
 			return ClusteredInstructions([save_instruction,mem_mov1,neg_instruction,mem_mov2,load_instruction])
 		
-		def spillAdd(name,val,liveness,current_instruction):
+		def spillAdd(name,val,liveness):
 			leftval = val[0]
 			rightval = val[1]
 			
@@ -78,37 +78,34 @@ class Translator:
 			load_instruction = MoveInstruction(getVariableInMemory(save_name),RegisterOperand("ebx"),"l")
 			return ClusteredInstructions([save_instruction,mem_mov,add_instruction,mem_mov2,load_instruction])
 			
-		def assignFunction(ast,liveness,current_instruction):
+		def assignFunction(ast,liveness):
 			name = ast.nodes[0].name
 			read = ast.expr
 			if isinstance(read,UnarySub):
-				return unaryFunction(name,read,liveness,current_instruction)
+				return unaryFunction(name,read,liveness)
 			elif isinstance(read,Add):
-				return addFunction(name,read,liveness,current_instruction)
+				return addFunction(name,read,liveness)
 			elif isinstance(read,Name):
-				return nameFunction(name,read,liveness,current_instruction)
+				return nameFunction(name,read,liveness)
 			elif isinstance(read,Const):
-				return constFunction(name,read,liveness,current_instruction)
+				return constFunction(name,read,liveness)
 			elif isinstance(read,CallFunc):
-				return callfuncFunction(name,read,liveness,current_instruction)
-			
+				return callfuncFunction(name,read,liveness)
 			else:
 				raise "Error: " + str(ast) + " currently not supported.\n"
 				
-		def unaryFunction(name,ast,liveness,current_instruction):
-			i = current_instruction						
-			val = translatePythonAST(ast.expr,liveness,i)
-			if isinstance(val,MemoryOperand) and isinstance(getName(name),MemoryOperand): return spillUnary(name,val,liveness,i)
+		def unaryFunction(name,ast,liveness):
+			val = translatePythonAST(ast.expr,liveness)
+			if isinstance(val,MemoryOperand) and isinstance(getName(name),MemoryOperand): return spillUnary(name,val,liveness)
 			mov_instruction = MoveInstruction(val,getName(name),"l")
 			neg_instruction = NegativeInstruction(getName(name),"l")
 			return ClusteredInstructions([mov_instruction] + [neg_instruction])
 			 
-		def addFunction(name,ast,liveness,current_instruction):
+		def addFunction(name,ast,liveness):
 
-			i = current_instruction
-			vals = translatePythonAST(ast,liveness,i)
+			vals = translatePythonAST(ast,liveness)
 						
-			if isinstance(getName(name),MemoryOperand): return spillAdd(name,vals,liveness,i)
+			if isinstance(getName(name),MemoryOperand): return spillAdd(name,vals,liveness)
 
 			leftval = vals[0]
 			rightval = vals[1]
@@ -176,7 +173,7 @@ class Translator:
 								if coloredgraph[x] == new_color:
 									save_name = x
 									return saveNameAdd(name,leftval,rightval,reg,save_name)
-					else: return spillAdd(name,vals,liveness,i)
+					else: return spillAdd(name,vals,liveness)
 						
 				coloredgraph[name] = "SPILL"
 				mem_mov = MoveInstruction(leftval,reg,"l")
@@ -190,21 +187,18 @@ class Translator:
 			return ClusteredInstructions([mov_instruction,add_instruction,mov2_instruction])
 	
 					
-		def nameFunction(name,ast,liveness,current_instruction):
-			i = current_instruction
-			val = translatePythonAST(ast,liveness,i)
-			if isinstance(val,MemoryOperand) and isinstance(getName(name),MemoryOperand): return spillName(name,val,liveness,i)			
+		def nameFunction(name,ast,liveness):
+			val = translatePythonAST(ast,liveness)
+			if isinstance(val,MemoryOperand) and isinstance(getName(name),MemoryOperand): return spillName(name,val,liveness)			
 			mov_instruction = MoveInstruction(val,getName(name),"l")
 			return ClusteredInstructions([mov_instruction])
 			
-		def constFunction(name,ast,liveness,current_instruction):
-			i = current_instruction
-			val = translatePythonAST(ast,liveness,i)
+		def constFunction(name,ast,liveness):
+			val = translatePythonAST(ast,liveness)
 			mov_instruction = MoveInstruction(val,getName(name),"l")
 			return ClusteredInstructions([mov_instruction])
 			
-		def callfuncFunction(name,ast,liveness,current_instruction):
-			i = current_instruction
+		def callfuncFunction(name,ast,liveness):
 			registers = []
 			
 			for x in liveness[1]:
@@ -223,8 +217,7 @@ class Translator:
 			 
 			return ClusteredInstructions(save + [call, mov_instruction] + load)
 		
-		def printFunction(ast,liveness,current_instruction):
-			i = current_instruction
+		def printFunction(ast,liveness):
 			registers = []
 			
 			for x in liveness[1]:
@@ -261,44 +254,44 @@ class Translator:
 			return assemblyInstructions
 					
 				
-		def translatePythonAST(ast,liveness=None,current_instruction=0):
+		def translatePythonAST(ast,liveness=None):
 			spill_vars = 0
-			if isinstance(ast,Module): return AssemblyProgram([translatePythonAST(ast.node,liveness,0)])
+			if isinstance(ast,Module): return AssemblyProgram([translatePythonAST(ast.node,liveness)])
 			
 			elif isinstance(ast,Stmt):
 				
 				x86 = []
 				for i in range(0,len(ast.nodes)):
-					x86.append(translatePythonAST(ast.nodes[i],liveness[i:i+2],current_instruction=i))
+					x86.append(translatePythonAST(ast.nodes[i],liveness[i:i+2]))
 				
 				x86 = removeTrivialMoves(x86)
 				return AssemblyFunction("main",x86,4*(len(memory)+1))
 				
-			elif isinstance(ast,Discard): return translatePythonAST(ast.expr,liveness,current_instruction)
+			elif isinstance(ast,Discard): return translatePythonAST(ast.expr,liveness)
 			
 			elif isinstance(ast,Const): return ConstantOperand(ast.value)
 			
-			elif isinstance(ast,Assign): return assignFunction(ast,liveness,current_instruction)
+			elif isinstance(ast,Assign): return assignFunction(ast,liveness)
 							
 			elif isinstance(ast,AssName): return getName(ast)
 				
 			elif isinstance(ast,Name): return getName(ast.name)
 			
-			elif isinstance(ast,Printnl): return printFunction(ast,liveness,current_instruction)
+			elif isinstance(ast,Printnl): return printFunction(ast,liveness)
 			
 			elif isinstance(ast,UnarySub):
-				neg_name = translatePythonAST(ast.expr,liveness,current_instruction)
+				neg_name = translatePythonAST(ast.expr,liveness)
 				if isinstance(neg_name,Operand):
 					x86AST = neg_name
 				return x86AST
 			
 			elif isinstance(ast,Add):
-				leftAST = translatePythonAST(ast.left,liveness,current_instruction)
-				rightAST = translatePythonAST(ast.right,liveness,current_instruction)
+				leftAST = translatePythonAST(ast.left,liveness)
+				rightAST = translatePythonAST(ast.right,liveness)
 				x86AST = [leftAST,rightAST]
 				return x86AST
 			
 			raise "Error: " + str(ast) + " currently not supported.\n"
 		print ast
-		t = translatePythonAST(ast,la,0)
+		t = translatePythonAST(ast,la)
 		return t
