b97e895dbd84e4060bd739e9418015d9d6ad19bd
arnopoulos <andrewarnopoulos@gmail.com>
2015-03-04 23:55:24 -0700

    Added removeDiscardMap function

--- Simplify.py
+++ Simplify.py
@@ -5,3 +5,8 @@ class Simplify():
 	def nameToBoolMap(ast):
 		if isinstance(ast,Name) and (ast.name == "True" or ast.name == "False"): return Boolean(ast.name == "True")
 		else: return ast
+
+	@staticmethod
+	def removeDiscardMap(ast):
+		if isinstance(ast,Discard): return ast.expr
+		else: return ast
