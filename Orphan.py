5063f055717a76f1c5a3ba91a891c598f1d102e3
arnopoulos <andrewarnopoulos@gmail.com>
2015-03-06 15:09:16 -0700

    Added Orphan

---
+++ Orphan.py
@@ -0,0 +1,20 @@
+from PythonASTExtension import *
+
+class Orphan():
+	def __init__(self,counter=0):
+		self.counter = counter
+
+	def getAndIncrement(self):
+		counter = self.counter
+		self.counter += 1
+		return counter
+
+	def findParentMap(self,ast):
+		if isinstance(ast,Stmt):
+			stmtArray =[]
+			for n in ast.nodes:
+				if not (isinstance(n,Printnl) or isinstance(n,Assign)):
+					stmtArray += [Assign([AssName("orphan"+str(self.getAndIncrement()),'OP_ASSIGN')],n)]
+				else: stmtArray += [n]
+			return Stmt(stmtArray)
+		else: return ast
