9a5d781d5549bc9e281a4d4be9616d676cef8844
arnopoulos <andrewarnopoulos@gmail.com>
2015-03-08 13:26:56 -0600

    Reordered traversal mappings

--- compile.py
+++ compile.py
@@ -9,9 +9,10 @@ from Explicate import *
 from Parser_hw2 import *
 import compiler
 from Simplify import *
-from TraverseIR import *
+from TraverseIR import TraverseIR
 from Namespace import *
 from Orphan import *
+from Functionize import *
 
 pythonFilename = sys.argv[1]
 
@@ -19,31 +20,22 @@ pythonFilename = sys.argv[1]
 #text_to_parse = file_to_parse.read()
 #raise Exception(text_to_parse)
 
-def removeNestedStmtMap(ast):
-	if isinstance(ast,Stmt):
-		stmtArray = []
-		for expr in ast.nodes:
-			stmtArray += expr.nodes if isinstance(expr,Stmt) else [expr]
-		return Stmt(stmtArray)
-	else: return ast
-
 pythonAST = compiler.parseFile(pythonFilename)
-# pythonAST = TraverseIR.map(pythonAST,Optimizer.reduceMap)
 pythonAST = TraverseIR.map(pythonAST,Simplify.removeDiscardMap)
-# print pythonAST
-pythonAST = TraverseIR.map(pythonAST,Optimizer.negationMap)
-pythonAST = TraverseIR.map(pythonAST,Namespace.removeDependenciesMap,Namespace(Namespace.environmentKeywords + Namespace.reservedKeywords))
 pythonAST = TraverseIR.map(pythonAST,Simplify.nameToBoolMap)
+pythonAST = TraverseIR.map(pythonAST,Optimizer.constantFoldingMap)
 pythonAST = TraverseIR.map(pythonAST,Orphan.findParentMap,Orphan())
+pythonAST = TraverseIR.map(pythonAST,Namespace.removeDependenciesMap,Namespace(Namespace.environmentKeywords + Namespace.reservedKeywords))
+
 pythonAST = TraverseIR.map(pythonAST,Explicate.explicateMap,Explicate())
+pythonAST = TraverseIR.map(pythonAST,Optimizer.explicateFoldingMap)
 pythonAST = TraverseIR.map(pythonAST,Explicate.removeIsTagMap)
-print pythonAST
-print
 pythonAST = TraverseIR.map(pythonAST,Flatten.flattenMap,Flatten())
-pythonAST = TraverseIR.map(pythonAST,removeNestedStmtMap)
+pythonAST = TraverseIR.map(pythonAST,Functionize.replaceBigPyobjMap)
+# pythonAST = TraverseIR.map(pythonAST,Functionize.replaceFunctionWithRuntimeMap)
+# print pythonAST
+pythonAST = TraverseIR.map(pythonAST,Flatten.removeNestedStmtMap)
 print pythonAST
-# flattenedAST = python_compiler.treeFlatten(explicatedAST)
-# print flattenedAST[0]
 # flattenedAST = pythonAST
 # x86AST = Translator.pythonASTToAssemblyAST(flattenedAST)
 
