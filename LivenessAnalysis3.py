ff6949810259920fd8359b71d49148f0f7f3edde
shadowfax6894 <anniekelly6894@gmail.com>
2015-03-10 11:29:37 -0600

    recursiongit add LivenessAnalysis3.py

--- LivenessAnalysis3.py
+++ LivenessAnalysis3.py
@@ -2,20 +2,59 @@ from compiler.ast import *
 import copy
 import Queue
 from PythonASTExtension import *
-from IfExpLivenessAnalysis import *
 
 class LivenessAnalysis:
 	
 	liveVariables = {}
 	
 	@staticmethod
-	def ifExpLiveness(ast):
+	def ifExpLiveness(ast,savenodes):
+		#savenodes = saveSet
+		#print ast
+		#savenodes = savenodes | set((ast.test.name,))
+		
+	
+				
+		if isinstance(ast,Assign):
+			if isinstance(ast.nodes[0],AssName): remove = set((ast.nodes[0].name))
+			elif isinstance(ast.nodes[0],Subscript): remove = (set((ast.nodes[0].expr.name)) | set((ast.nodes[0].subs[0].name)))
+			if isinstance(ast.expr,Name): (savenodes - remove) | set((ast.expr.name))
+			elif isinstance(ast.expr,Const): (savenodes - remove)
+			elif isinstance(ast.expr,CallFunc): (savenodes - remove) | LivenessAnalysis.callFuncLiveness(ast.expr)
+			elif isinstance(ast.expr,UnarySub): (savenodes - remove) | set((ast.expr.expr.name))
+			elif isinstance(ast.expr,Add): (savenodes - remove) | set((ast.expr.left.name)) | set((ast.expr.right.name))
+			elif isinstance(ast.expr,Subscript): (savenodes - remove) | set((ast.expr.expr.name)) | set((ast.expr.subs[0].name))
+			elif isinstance(ast.expr,Dict): (savenodes - remove) | set([x.name for x in ast.expr.items])
+			elif isinstance(ast.expr,List): (savenodes - remove) | set([x.name for x in ast.expr.nodes])
+			elif isinstance(ast.expr,Not): (savenodes - remove) | set((ast.expr.expr.name))
+			elif isinstance(ast.expr,And): (savenodes - remove) | set([x.name for x in ast.expr.nodes])
+			elif isinstance(ast.expr,Or): (savenodes - remove) | set([x.name for x in ast.expr.nodes])
+			elif isinstance(ast.expr,Compare):
+				savevar = set()
+				savevar2 = set()
+				if isinstance(ast.expr.ops[0][1],Name): savevar2 = set((ast.expr.ops[0][1].name,))
+				if isinstance(ast.expr.expr,Name): savevar = set((ast.expr.expr.name,))
+				(savenodes - remove) | savevar | savevar2
+			elif isinstance(ast.expr,InjectFrom): (savenodes - remove) | set((ast.expr.arg))
+			elif isinstance(ast.expr,ProjectTo): (savenodes - remove) | set((ast.expr.arg))				
+			else: raise Exception("Error: Unrecognized node type")										
+		elif isinstance(ast,CallFunc): savenodes | LivenessAnalysis.callFuncLiveness(ast)
+		elif isinstance(ast,GetTag): savenodes
+		elif isinstance(ast,IfExp):
+			savenodes = savenodes | set((ast.test.name,))			
+			if isinstance(ast.then,Stmt):
+				for node in ast.then.nodes:
+					savenodes = savenodes | LivenessAnalysis.ifExpLiveness(node,savenodes)
+			else: savenodes = savenodes | LivenessAnalysis.ifExpLiveness(ast.then,savenodes)
+				
+			if isinstance(ast.else_,Stmt):
+				for node in ast.else_.nodes:
+					savenodes = savenodes | LivenessAnalysis.ifExpLiveness(node,savenodes)
+			else: savenodes = savenodes | LivenessAnalysis.ifExpLiveness(ast.else_,savenodes)	
 
-		def recursion(ast):
-			if isinstance(ast.then,stmt):
-				save1 = [determineLiveVariables(node,j) for node in ast.then.nodes]
-			if isinstance(ast.else_,stmt):
-				save2 = [determineLiveVariables(node,j) for node in ast.else_.nodes]
+		elif isinstance(ast,Printnl): savenodes | set((ast.nodes[0]))
+		else: raise Exception("Error: Unrecognized node type")		
+		return savenodes
 				
 
 	@staticmethod
@@ -44,14 +83,22 @@ class LivenessAnalysis:
 			elif isinstance(ast.expr,Not): return (LivenessAnalysis.liveVariables[j+1] - remove) | set((ast.expr.expr.name))
 			elif isinstance(ast.expr,And): return (LivenessAnalysis.liveVariables[j+1] - remove) | set([x.name for x in ast.expr.nodes])
 			elif isinstance(ast.expr,Or): return (LivenessAnalysis.liveVariables[j+1] - remove) | set([x.name for x in ast.expr.nodes])
-			elif isinstance(ast.expr,Compare): return (LivenessAnalysis.liveVariables[j+1] - remove) | set((ast.expr.expr.name)) | set((ast.expr.ops[1].name))
-			#elif isinstance(ast.expr,IfExp): return (LivenessAnalysis.liveVariables[j+1] - remove) | LivenessAnalysis.ifExpLiveness(ast.expr)
-			elif isinstance(ast.expr,InjectFrom): return (LivenessAnalysis.liveVariables[j+1] - remove) | set((ast.expr.arg))
+			elif isinstance(ast.expr,Compare):
+				savevar = set()
+				savevar2 = set()
+				if isinstance(ast.expr.ops[0][1],Name): savevar2 = set((ast.expr.ops[0][1].name,))
+				if isinstance(ast.expr.expr,Name): savevar = set((ast.expr.expr.name,))				
+				return (LivenessAnalysis.liveVariables[j+1] - remove) | savevar | savevar2
+			elif isinstance(ast.expr,InjectFrom):
+				if isinstance(ast.expr.arg,Name): return (LivenessAnalysis.liveVariables[j+1] - remove) | set((ast.expr.arg.name,))
+				else: return (LivenessAnalysis.liveVariables[j+1] - remove)
 			elif isinstance(ast.expr,ProjectTo): return (LivenessAnalysis.liveVariables[j+1] - remove) | set((ast.expr.arg))				
 			else: raise Exception("Error: Unrecognized node type")										
 		elif isinstance(ast,CallFunc): return LivenessAnalysis.liveVariables[j+1] | LivenessAnalysis.callFuncLiveness(ast)
 		elif isinstance(ast,GetTag): return LivenessAnalysis.liveVariables[j+1]
-		elif isinstance(ast,IfExp): return LivenessAnalysis.liveVariables[j+1] | LivenessAnalysis.ifExpLiveness(ast.expr)
+		elif isinstance(ast,IfExp):
+			savenodes = set()
+			return LivenessAnalysis.liveVariables[j+1] | LivenessAnalysis.ifExpLiveness(ast,savenodes)
 		elif isinstance(ast,Printnl): return LivenessAnalysis.liveVariables[j+1] | set((ast.nodes[0]))
 		else: raise Exception("Error: Unrecognized node type")
 	
