c934895cb18d12219c591a0acaca5f9492fa3816
arnopoulos <andrewarnopoulos@gmail.com>
2015-03-11 14:07:43 -0600

    Maybe fixed align instruction

--- AssemblyAST/X86GeneralInstructions.py
+++ AssemblyAST/X86GeneralInstructions.py
@@ -52,6 +52,7 @@ class AlignInstruction(BinaryInstruction):
 		return x86InstructionToString(self.__class__.__name__,[self.fromOperand,self.toOperand])
 
 	def printInstruction(self):
+		return ".align " + str(self.fromOperand.value.value) + ", " + self.toOperand.printOperand()
 		return printBinaryX86Instruction(".align", self.fromOperand, self.toOperand)
 
 class EntryPointInstruction(UnaryInstruction):
