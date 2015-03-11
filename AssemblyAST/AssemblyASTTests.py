3cfa7fb455e29cb7ea5a5c228efd654a288c89c1
arnopoulos <andrewarnopoulos@gmail.com>
2015-03-10 22:41:50 -0600

    Added Unit Testing, that doesn't work

---
+++ AssemblyAST/AssemblyASTTests.py
@@ -0,0 +1,21 @@
+from X86ArithmeticInstructions import *
+# import unittest
+
+# import X86ArithmeticInstructions
+from X86Instructions import *
+
+# class TestInitialization(unittest.TestCase):
+
+# 	def setUp(self):
+# 		pass
+
+# 	def test_emptyClusteredInstruction(self):
+# 		X86ArithmeticInstructions.ClusteredInstruction()
+
+# 	def test_CallInstruction(self):
+# 		X86ArithmeticInstructions.CallInstruction(NameOperand("dfjkdslf"))
+
+
+if __name__ == '__main__':
+	# unittest.main()
+	print ClusteredInstruction()
