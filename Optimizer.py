94307fa710fa619a88dc350ef9577b785cfb1f75
arnopoulos <andrewarnopoulos@gmail.com>
2015-03-12 13:33:51 -0600

    Optimized Compare

--- Optimizer.py
+++ Optimizer.py
@@ -30,6 +30,9 @@ class Optimizer:
 			if isinstance(ast.expr,Const) or isinstance(ast.expr,Boolean):
 				return Boolean(not ast.expr.value)
 			else: return ast
+		elif isinstance(ast,Compare):
+			if (isinstance(ast.expr,Const) or isinstance(ast.expr,Boolean)) and (isinstance(ast.ops[0][1],Const) or isinstance(ast.ops[0][1],Boolean)):
+				return Boolean(ast.expr == ast.ops[0][1])
 		else: return ast
 
 	@staticmethod
