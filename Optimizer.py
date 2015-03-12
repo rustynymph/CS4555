292f2913a46fac6383881629569f23196868c2f2
arnopoulos <andrewarnopoulos@gmail.com>
2015-03-12 15:28:30 -0600

    Fixed bug

--- Optimizer.py
+++ Optimizer.py
@@ -32,8 +32,19 @@ class Optimizer:
 			else: return ast
 		elif isinstance(ast,Compare):
 			if (isinstance(ast.expr,Const) or isinstance(ast.expr,Boolean)) and (isinstance(ast.ops[0][1],Const) or isinstance(ast.ops[0][1],Boolean)):
-				return Boolean((ast.expr == ast.ops[0][1]) * 4 + 1)
+				return Boolean((ast.expr == ast.ops[0][1]))
 			else: return ast
+		elif isinstance(ast,IfExp):
+			if isinstance(ast.test,Const):
+				print ast
+				if ast.test.value // 4: return ast.then
+				else: return ast.else_
+			elif isinstance(ast.test,Boolean):
+				print ast
+				if ast.test.value: 
+					print "yeah"
+					return ast.then
+				else: return ast.else_
 		else: return ast
 
 	@staticmethod
