d651e16545a7785cce1687cb89596cdd68d4bf36
shadowfax6894 <anniekelly6894@gmail.com>
2015-03-18 19:04:42 -0600

    heap heap

--- Heapify.py
+++ Heapify.py
@@ -5,7 +5,7 @@ class Heapify(node):
 	
 	def __init__(self,IR):
 		self.freeVariables = {}
-		self.IR = IR.node.nodes
+		self.IR = IR
 		self.heapifySet = set()
 	
 	def freeVars(self,node):
@@ -80,23 +80,59 @@ class Heapify(node):
 	
 	def needHeapification(self,node):
 		if isinstance(node,Lambda):
-			if isinstance(node.code,Lambda):
-				self.heapifySet = self.heapifySet |  self.needHeapification(node.code)
-			else: 
-				if isinstance(node.code,Stmt):
-					save = set()
-					for node in node.code.nodes:
-						save = save | self.freeVars(node)
-					self.heapifySet = self.heapifySet | save
-				else: self.heapifySet = self.heapifySet | self.freeVars()
+			if isinstance(node.code,Stmt):
+				save = set()
+				for x in node.code.nodes:
+					if isinstance(x,Lambda): save = save |  self.needHeapification(x)
+					else: save = save | self.freeVars(x)
+				self.heapifySet = self.heapifySet | save
+			elif isinstance(node.code,Lambda): return self.heapifySet = self.heapifySet | self.needHeapification(node.code)
+			else: self.heapifySet = self.heapifySet | self.freeVars(node)
+		else: return self.heapifySet
 	
-	def heapify(self,ast):
-		if isinstance(ast,Name):
-			if ast.name in self.heapifySet: return Subscript(ast.name,FLAGS,[Const(DecimalValue(0))])
-		elif isinstance(ast,Assign):
-			if isinstance(ast.nodes[0],AssName) and isinstance(ast.expr,Name):
-				if ast.expr.name in self.heapifySet:
-					return Assign(Subscript(ast.name,FLAGS,[Const(DecimalValue(0))]),self.heapify(ast.expr))
-		else: return ast
+	def heapify(self,node):
+		#Name and Assign are base cases
+		if isinstance(node,Name):
+			if node.name in self.heapifySet: return Subscript(node.name,FLAGS,[Const(DecimalValue(0))])
+			else: return node
+		elif isinstance(node,Assign):
+			if isinstance(node.nodes[0],AssName) and isinstance(node.expr,Name):
+				if node.expr.name in self.heapifySet:
+					return Assign(Subscript(node.name,FLAGS,[Const(DecimalValue(0))]),self.heapify(node.expr))
+			else: return node
+		if isinstance(node,Const): return node
+		elif isinstance(node,Add): return Add(self.heapify(node.left),self.freeVars(node.right))
+		elif isinstance(node,CallFunc): return CallFunc(node.name,[self.heapify(e) for e in node.args])
+		elif isinstance(node,Lambda):
+			save = []
+			if isinstance(node.code,Stmt):
+				save += [self.heapify(x) for x in node.code.nodes]
+			else: save += self.heapify(node.code)
+			return Lambda(node.argnames,save)
+		elif isinstance(node,UnarySub): return UnarySub(self.heapify(node.expr))
+		elif isinstance(node,List): return List([self.heapify(e) for e in node.nodes])	
+		elif isinstance(node,Dict): return Dict([(self.heapify(e[0]),self.heapify(e[1])) for e in node.items])
+		elif isinstance(node,Subscript): return Subscript(self.heapify(node.expr),[self.heapify(e) for e in node.subs])
+		elif isinstance(node,IfExp):
+			savethen = []
+			saveelse = []
+			if isinstance(node.then,Stmt): savethen += [self.heapify(e) for e in node.then.nodes]
+			else: savethen = self.heapify(node.then)
+			if isinstance(node.else_,Stmt): saveelse += [self.heapify(e) for e in node.else_.nodes]
+			else: saveelse = self.heapify(node.else_)
+			return IfExp(self.heapify(ast.test),savethen,saveelse)	
+		elif isinstance(node,Compare): return Compare(self.heapify(node.expr),self.freeVars(node.ops[0][1]))
+		elif isinstance(node,And): return And([self.heapify(e) for e in node.nodes])
+		elif isinstance(node,Or): return Or([self.heapify(e) for e in node.nodes])	
+		elif isinstance(node,Let): return Let(self.heapify(node.var),self.heapify(node.expr),self.heapify(node.body))		
+		elif isinstance(node,InjectFrom): return InjectFrom(node.typ,self.heapify(node.arg))
+		elif isinstance(node,GetTag): return GetTag(self.heapify(node.arg))
+		elif isinstance(node,ProjectTo): return ProjectTo(node.typ,self.heapify(node.arg))
+		elif isinstance(node,Not): return Not(self.heapify(node.expr))
+		elif isinstance(node,Printnl): return Printnl(self.heapify(node.nodes[0]))
+		else: return node
 
-	def heapifyInstructions(self,IR): return [self.heapify(i) for i in self.IR]
+	def heapifyInstructions(self):
+		self.needHeapification(instruction) for instruction in self.IR.node.nodes
+		stmt = [self.heapify(i) for i in self.IR.node.nodes]
+		return Module(stmt)
