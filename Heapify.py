f822b7076bf665a38971cfb574f11cccfbc6307d
shadowfax6894 <anniekelly6894@gmail.com>
2015-03-19 17:30:49 -0600

    workin

--- Heapify.py
+++ Heapify.py
@@ -5,21 +5,18 @@ from AssemblyAST import *
 class Heapify:
 	
 	def __init__(self,fvs):
-		self.heapifySet = fvs
+		self.freeVariables = fvs
 
 	def heapify(self,node):
 		if isinstance(node,Name):
-			if node.name in self.heapifySet:
-				return Subscript(Name(node.name),'OP_APPLY',[Const(DecimalValue(0))])
+			if node.name in self.freeVariables:	return Subscript(Name(node.name),'OP_APPLY',[Const(DecimalValue(0))])
 			else: return node
 		elif isinstance(node,Assign):
 			if isinstance(node.nodes[0],AssName):
-				if node.nodes[0].name in self.heapifySet:
+				if node.nodes[0].name in self.freeVariables:
 					listcreate = Assign([AssName(Name(node.nodes[0].name),'OP_ASSIGN')],CallFunc('create_list',[1]))
 					subassign = Assign(Subscript(Name(node.nodes[0].name),'OP_ASSIGN',[Const(DecimalValue(0))]),node.expr)
 					return Stmt([listcreate,subassign])
-			else:
-				print node
-				return node
+			else: return node
 		return node
 
