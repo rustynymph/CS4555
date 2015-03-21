9b24342f2fbfb212a9e94b580e635241bcb90348
shadowfax6894 <anniekelly6894@gmail.com>
2015-03-21 11:03:00 -0600

    function revert

---
+++ FunctionRevert.py
@@ -0,0 +1,20 @@
+from PythonASTExtension import *
+from AssemblyAST import *
+
+class FunctionRevert:
+
+	@staticmethod
+	def revert(node):
+		if isinstance(node,Assign):
+			if isinstance(node.expr,Lambda):
+				if isinstance(node.nodes[0],AssName):
+					funny = Function(None,node.nodes[0].name,node.expr.argnames,(),0,None,node.expr.code) 
+					funny.uniquename = node.expr.uniquename
+					return funny
+				elif isinstance(node.nodes[0],Subscript):
+					funny = Function(None,node.nodes[0],node.expr.argnames,(),0,None,node.expr.code)
+					funny.uniquename = node.expr.uniquename
+					return funny
+				else: return node
+			else: return node
+		else: return node
