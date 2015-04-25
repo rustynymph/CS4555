c76e26944eb0dbc4743404b04f4151f0369a6c26
shadowfax6894 <anniekelly6894@gmail.com>
2015-04-25 17:20:53 -0600

    test files

---
+++ test200.py
@@ -0,0 +1,45 @@
+x = input()
+print x
+y = input()
+print y
+z = input()
+print z
+a = input() + input() + input() + input() + input()
+print a
+b = x + y + z + a
+print b
+x = input()
+print x
+y = input()
+print y
+z = input()
+print z
+a = input() + input() + input() + input() + input()
+print a
+b = x + y + z + a
+print b
+print b
+print a
+x = input()
+print x
+y = input()
+print y
+z = input()
+print z
+a = input() + input() + input() + input() + input()
+print a
+b = x + y + z + a
+print b
+x = input()
+print x
+y = input()
+print y
+z = input()
+print z
+a = input() + input() + input() + input() + input()
+print a
+b = x + y + z + a
+print b
+print b
+print a
+
